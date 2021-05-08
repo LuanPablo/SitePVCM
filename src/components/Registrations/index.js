228100180af901c979251cd35fc0af761962b1b0
Luan Pablo Paiva de Lima <luan.lima@radixeng.com.br>
2021-05-08 15:12:21 -0300

    registrations

--- src/components/Registrations/index.js
+++ src/components/Registrations/index.js
@@ -1,7 +1,33 @@
 import { Container } from "./styles";
+import aberturaVagas from "../../assets/img1.svg";
+import prematricula from "../../assets/img2.svg";
+import matricula from "../../assets/img3.svg";
 
 export function Registrations() {
   return <Container>
-    Registration
+    <h2>COMO SÃO FEITAS AS INSCRIÇÕES?</h2>
+    <div>
+      <div>
+        <img src={aberturaVagas} alt="abertura de vagas" />
+        <h3>Abertura de Vagas</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar habitant odio diam, ac natoque sed platea. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar habitant odio diam, ac natoque sed platea.
+        </p>
+      </div>
+      <div>
+        <img src={prematricula} alt="" />
+        <h3>Pré-inscrição</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar habitant odio diam, ac natoque sed platea. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar habitant odio diam, ac natoque sed platea.
+        </p>
+      </div>
+      <div>
+        <img src={matricula} alt="" />
+        <h3>Matrícula</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar habitant odio diam, ac natoque sed platea. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar habitant odio diam, ac natoque sed platea.
+        </p>
+      </div>
+    </div>
   </Container>;
 }
--- src/components/Registrations/styles.js
+++ src/components/Registrations/styles.js
@@ -1,5 +1,43 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+
+max-width:1080px;
+margin:auto;
+
+h2{
+  color: var(--primary);
+  font-weight: 900;
+  font-size:1.5rem;
+  margin:4rem 0 4rem 0;
+  text-align: center;
+}
+>div{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap:3rem;
+  display: flex;
+  justify-content:center;
+  text-align: center;
+  line-height: 4.5rem;
+ 
+  img{
+  width: 5rem;
+  }
+  h3{
+  color:#FF9657;
+  font-weight: bold;
+  font-size: 1.2rem;
+  line-height: 3.5rem;
+  }
+  p{
+  width:18rem; 
+  font-weight: normal;
+  font-size: 0.8rem;
+  line-height: 1.5rem;
+  display: flex;
+  text-align:justify;
   
+  }
+}
 `;
